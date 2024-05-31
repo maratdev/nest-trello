dca136a9f4550b9d1536afbd6218593fd6e06f0f
maratdev <starspersona@gmail.com>
2024-05-31 23:14:49 +0300

    feat: authorization service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	Res,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+		return this.authService.login(dto);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('register')
+	async register(
+		@Body() dto: AuthDto,
+		@Res({ passthrough: true }) res: Response,
+	) {
+		const { refreshToken, ...response } = await this.authService.register(dto);
+		this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+		return response;
+	}
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJwtConfig } from 'src/config/jwt.config';
+import { AuthController } from './auth.controller';
+
+@Module({
+	imports: [
+		UserModule,
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig,
+		}),
+	],
+	providers: [AuthService],
+	controllers: [AuthController],
+})
+export class AuthModule {}
