5df35dc221965b3ed40166305b8a1af3da797298
maratdev <starspersona@gmail.com>
2024-06-04 22:54:07 +0300

    feat: Transaction

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { TimeBlockController } from './time-block/time-block.controller';
+import { TimeBlockModule } from './time-block/time-block.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule],
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		TaskModule,
+		TimeBlockModule,
+	],
+	controllers: [TimeBlockController],
 })
 export class AppModule {}
